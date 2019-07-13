de17bd95f253b5207eb3a89001d16b343118b6a4
Anubhav177 <anubhav177@gmail.com>
2019-07-13 08:21:29 +0530

    Arduino-Computer Interface

--- UI.py
+++ UI.py
@@ -1,16 +1,15 @@
 import serial
 import sys
 import time
-
+# Initialize Comms
 ser=serial.Serial(input(),baudrate=9600,timeout=1)
 
+#some functions we might need for sending stuff from the computer to the board
+def convLongStr(num):
+    return ''.join([chr(int(char,base=16)) for char in hex(num).strip('0x')])
 
-def msgReceived():
-    pass
-
-
-def error():
-    pass
+def convStrLong(Str):
+    return int('0x'+''.join([hex(ord(char)).strip('0x') for char in Str]),base=16)
 
 def __xor__(arr):
     if len(arr) is 0:
@@ -21,31 +20,69 @@ def __xor__(arr):
     return sum
 
 def serialListenError():
-    while(ser.in_waiting):
-        ser.read_until('\n')
-    ser.write(b"ERROR")
+    if ser.in_waiting>0:
+        ser.reset_input_buffer()
+    ser.write("ERROR".encode('ascii'))
+##################################################################################
+#Initialization functions - Only with the very basic functionalities!!!
+def setfr(num):
+    sendMsg('SETFR'+convLongStr(num))
+
+def cgstp(num):
+    sendMsg('CGSTP'+convLongStr(num))
+
+def mlstp(num):
+    sendMsg('MLSTP'+convLongStr(num))
+
+def chkcf(): #Should'nt this be CHCKF?????
+    sendMsg('CHKCF')
+
+def genlo():
+    sendMsg('GENLO')
+
+def error(msg):
+    sendMsg('ERROR'+msg)
+
+def setgn(num):
+    sendMsg('ERROR'+convLongStr(num))
+
+##################################################################################
+#Functions for parsing packets, along with the verification protocol
+
 
 def sendMsg(msg):
     a=chr(__xor__([ord(char) for char in msg]))
     b=chr(sum([ord(char) for char in msg])%256)
 
     while(True): 
+        ser.reset_input_buffer()
         ser.write(("ST"+msg+a+b+'\r\n').encode('ascii'))
         time.sleep(0.01)
         if ser.in_waiting:
-            if ser.read()=='C':
+            FL=ser.read()
+            if FL=='C':
                 if ser.read()=='N':
                     if ser.read()=='F':
                         if ser.read()==a:
                             break
-        while ser.in_waiting:
-            ser.read()
+            elif FL=='E':
+                pass
+            else:
+                return None
+
+        ser.reset_input_buffer()
+
+def msgReceived(msg):
+    if (msg[0:5]=='SDDAT'):
+        pass #Worry about this later.
+    elif (msg[0:5]=='ERROR'):
+        pass #Decide upon its contents later.  
 
 def listener():
     ser.flush()
-    if ser.in_waiting:
+    if ser.in_waiting>0:
         run=True
-        if ser.in_waiting:
+        if ser.in_waiting>0:
             run = True
         if ser.read()=='S':
             if ser.read()=='T':
@@ -69,18 +106,14 @@ def listener():
                 if (Str[n-1]==b) and (Str[n-2]==a):
                     ser.write(('CNF'+a).encode('ascii'))
                 # The msg received is a bit different. Will implement later
-                    msgReceived()
+                    #msgReceived()
                     return None
                 else:
                     serialListenError()
             else:
                 serialListenError()
                 return None    
-
-while 1:
+##################################################################################
+while True:
     listener()
-    time.sleep(0.1)
-
-
-
-
+    time.sleep(0.01)
